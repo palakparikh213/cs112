e672aeb0b22c78a9233034aeaa0a1d9fef787e82
Palak Parikh <77861758+palakparikh213@users.noreply.github.com>
2023-03-13 21:24:52 -0400

    Update Trie.java

--- Trie.java
+++ Trie.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 /**
  * This class implements a Trie. 
  * 
- * @author Sesh Venugopal
+ * @author Palak Parikh palakparikh213@gmail.com pdp89
  *
  */
 public class Trie {
@@ -15,7 +15,7 @@ public class Trie {
     
     /**
      * Builds a trie by inserting all words in the input array, one at a time,
-     * in sequence FROM FIRST TO LAST. (The sequence is IMPORTANT!)
+     * in sequence FROM FIRST TO prev. (The sequence is IMPORTANT!)
      * The words in the input array are all lower case.
      * 
      * @param allWords Input array of words (lowercase) to be inserted.
@@ -23,74 +23,88 @@ public class Trie {
      */
     public static TrieNode buildTrie(String[] allWords) {
         /** COMPLETE THIS METHOD **/
-           TrieNode root = new TrieNode(null, null, null);
-           if(allWords.length == 0)
-               return root; //if no words, returns null tree
-           root.firstChild = new TrieNode(new Indexes(0,(short)(0),(short)(allWords[0].length() - 1)), null, null);
-           TrieNode pointer = root.firstChild, prev = root.firstChild;
-           int s = -1, startIndex = -1, endIndex = -1, wordIndex = -1;
-           for(int i = 1; i < allWords.length; i++) {
-               String word = allWords[i];
-                   while(pointer != null) {
-                   startIndex = pointer.substr.startIndex;
-                   endIndex = pointer.substr.endIndex;
-                   wordIndex = pointer.substr.wordIndex;
-                   if(startIndex > word.length()) {
-                       prev = pointer;
-                       pointer = pointer.sibling;
-                       continue;
-                   }
-                   s = common(allWords[wordIndex].substring(startIndex, endIndex+1),
-                           word.substring(startIndex));
-                   if(s != -1)
-                       s += startIndex;
-                   if(s == -1) {
-                       prev = pointer;
-                       pointer = pointer.sibling;
-                   }
-                   else {
-                       if(s == endIndex) {
-                           prev = pointer;
-                           pointer = pointer.firstChild;
-                       }
-                       else if (s < endIndex){
-                           prev = pointer;
-                           break;
-                       }
-                   }
-               }
-               if(pointer == null) {
-                   Indexes alpha = new Indexes(i, (short)startIndex, (short)(word.length()-1));
-                   prev.sibling = new TrieNode(alpha, null, null);
-               } else {
-                   Indexes currentIndexes = prev.substr;
-                   TrieNode currentFirstChild = prev.firstChild;
-                   Indexes currWordNewIndexes = new Indexes(currentIndexes.wordIndex, (short)(s+1), currentIndexes.endIndex);
-                   currentIndexes.endIndex = (short)s;
-                   prev.firstChild = new TrieNode(currWordNewIndexes, null, null);
-                   prev.firstChild.firstChild = currentFirstChild;
-                   prev.firstChild.sibling = new TrieNode(new Indexes((short)i,
-                           (short)(s+1),
-                           (short)(word.length()-1)),
-                           null, null);
-               }
-              
-               pointer = prev = root.firstChild;
-               s = startIndex = endIndex = wordIndex = -1;
-           }
-          
-           return root;
-          
-       }
-       private static int common(String alpha, String beta){
-           int ans = 0;
-           while(ans<alpha.length() && ans<beta.length() && alpha.charAt(ans)==beta.charAt(ans)){
-               ans++;
-           }
-           return (ans-1);
-       }
+        
         // FOLLOWING LINE IS A PLACEHOLDER TO ENSURE COMPILATION
-        // MODIFY IT AS NEEDED FOR YOUR IMPLEMENTATION
+        TrieNode root = new TrieNode (null,null,null); //creates null root node
+ 
+        //returns null root node if input array is empty
+        if (allWords==null) {
+            return root;
+        }
+
+        
+            //initializes first child
+            Indexes ind = new Indexes(0, (short)(0), (short)(allWords[0].length()-1));
+            root.firstChild = new TrieNode(ind,null,null);
+            //initializes pointer
+            TrieNode ptr = root.firstChild;
+            //initializes previous node
+            TrieNode prev = root.firstChild;
+            
+            int k = -1;
+            int wordIndex = -1;
+            int startIndex = -1;
+            int endIndex = -1;
+            
+            //  String word = " ";
+            
+            for(int index = 1; index < allWords.length; index++) {
+                String word = allWords[index];
+            while(ptr != null) {
+                //ends loop if pointer becomes null and nothing matches
+                startIndex = ptr.substr.startIndex;
+                endIndex = ptr.substr.endIndex;
+                wordIndex = ptr.substr.wordIndex;
+
+                //traversing while checking if startIndex<word.length
+                if(startIndex > word.length()) {
+                    prev = ptr;
+                    ptr = ptr.sibling;
+                    continue;
+                }
+
+                int p = 0;
+            while((p < (allWords[wordIndex].substring(startIndex, endIndex+1)).length()) && p < (word.substring(startIndex).length()) && (allWords[wordIndex].substring(startIndex, endIndex+1)).charAt(p) == (word.substring(startIndex).charAt(p))) {
+                p++;
+            }
+                k=p-1;
+                if(k!= -1) {
+                    k+= startIndex;
+                }
+                if(k== -1) {
+                    prev = ptr;
+                    ptr = ptr.sibling;
+                }
+                else {
+                    if(k== endIndex) { 
+                        prev = ptr;
+                        ptr = ptr.firstChild;
+                    }
+                    else if (k < endIndex){ 
+                        prev = ptr;
+                        break;
+                    }
+                }
+            }
+            
+            if(ptr == null) {
+                Indexes indexes = new Indexes(index, (short)startIndex, (short)(word.length()-1));
+                prev.sibling = new TrieNode(indexes, null, null);
+            } 
+            else {
+                Indexes currentIndex = prev.substr; 
+                TrieNode currentFS = prev.firstChild; 
+                Indexes currentWordIndexes = new Indexes(currentIndex.wordIndex, (short)(k+1), currentIndex.endIndex);
+                currentIndex.endIndex = (short)k;    
+                prev.firstChild = new TrieNode(currentWordIndexes, null, null);
+                prev.firstChild.firstChild = currentFS;
+                prev.firstChild.sibling = new TrieNode(new Indexes((short)index, (short)(k+1), (short)(word.length()-1)), null, null);
+            }
+            ptr = prev = root.firstChild;
+            k = startIndex = endIndex = wordIndex = -1;
+        }
+        return root;
+    }
 
     
     /**
@@ -100,7 +114,7 @@ public class Trie {
      * the completion list for prefix "b" would be the leaf nodes that hold "bear", "bull", and "bell"; 
      * for prefix "be", the completion would be the leaf nodes that hold "bear" and "bell", 
      * and for prefix "bell", completion would be the leaf node that holds "bell". 
-     * (The last example shows that an input prefix can be an entire word.) 
+     * (The prev example shows that an input prefix can be an entire word.) 
      * The order of returned leaf nodes DOES NOT MATTER. So, for prefix "be",
      * the returned list of leaf nodes can be either hold [bear,bell] or [bell,bear].
      *
@@ -113,33 +127,36 @@ public class Trie {
      */
     public static ArrayList<TrieNode> completionList(TrieNode root,String[] allWords, String prefix) {
         /** COMPLETE THIS METHOD **/
-         if(root==null){
-               return null;
-           } 
-           ArrayList<TrieNode> answer = new ArrayList<TrieNode>();
-           TrieNode pointer = root;
-           while(pointer!=null){
-               if(pointer.substr==null){
-                   pointer = pointer.firstChild;
-               }
-           String s = allWords[pointer.substr.wordIndex];
-           String alpha = s.substring(0, pointer.substr.endIndex+1);
-           if(s.startsWith(prefix) || prefix.startsWith(alpha)){
-               if(pointer.firstChild!=null){
-                   answer.addAll(completionList(pointer.firstChild,allWords,prefix));
-                   pointer=pointer.sibling;
-               }else{
-                   answer.add(pointer);
-                   pointer=pointer.sibling;
-               }
-           }else{
-               pointer = pointer.sibling;
-           }
-           return answer;
-       }
-        // FOLLOWING LINE IS A PLACEHOLDER TO ENSURE COMPILATION
-        // MODIFY IT AS NEEDED FOR YOUR IMPLEMENTATION
-        return null;
+        if (root==null) {
+            return null;
+        }
+        ArrayList<TrieNode> matches = new ArrayList<>();
+        TrieNode ptr = root;
+        while (ptr != null) {
+            if(ptr.substr == null) {
+                ptr = ptr.firstChild;
+            }
+            
+            String words = allWords[ptr.substr.wordIndex];
+            String a = words.substring(0, ptr.substr.endIndex+1);
+            
+            if (words.startsWith(prefix) || prefix.startsWith(a)) {
+                if(ptr.firstChild != null) { 
+                    matches.addAll(completionList(ptr.firstChild, allWords, prefix));
+                    ptr = ptr.sibling;
+                } 
+                else { 
+                    matches.add(ptr);
+                    ptr = ptr.sibling;
+                }
+            } 
+            else {
+                ptr = ptr.sibling;
+            }
+        }
+        
+        return matches;
+
     }
     
     public static void print(TrieNode root, String[] allWords) {
@@ -160,6 +177,7 @@ public class Trie {
                             .substring(0, root.substr.endIndex+1);
             System.out.println("      " + pre);
         }
+        
         for (int i=0; i < indent-1; i++) {
             System.out.print("    ");
         }
@@ -170,12 +188,12 @@ public class Trie {
             System.out.println(root.substr);
         }
         
-        for (TrieNode pointer=root.firstChild; pointer != null; pointer=pointer.sibling) {
+        for (TrieNode ptr=root.firstChild; ptr != null; ptr=ptr.sibling) {
             for (int i=0; i < indent-1; i++) {
                 System.out.print("    ");
             }
             System.out.println("     |");
-            print(pointer, indent+1, words);
+            print(ptr, indent+1, words);
         }
     }
  }
